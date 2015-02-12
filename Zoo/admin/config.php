d8e831602cfcad95408a1266582935dd3621380a
Stas <stas249501@gmail.com>
2015-02-12 12:05:58 +0200

    fix config

--- Zoo/admin/config.php
+++ Zoo/admin/config.php
@@ -8,17 +8,17 @@ define('HTTPS_SERVER', 'http://109.87.116.253/admin/');
 define('HTTPS_CATALOG', 'http://109.87.116.253/');
 
 // DIR
-define('DIR_APPLICATION', 'D:\zoo/admin/');
-define('DIR_SYSTEM', 'D:\zoo/system/');
-define('DIR_DATABASE', 'D:\zoo/system/database/');
-define('DIR_LANGUAGE', 'D:\zoo/admin/language/');
-define('DIR_TEMPLATE', 'D:\zoo/admin/view/template/');
-define('DIR_CONFIG', 'D:\zoo/system/config/');
-define('DIR_IMAGE', 'D:\zoo/image/');
-define('DIR_CACHE', 'D:\zoo/system/cache/');
-define('DIR_DOWNLOAD', 'D:\zoo/download/');
-define('DIR_LOGS', 'D:\zoo/system/logs/');
-define('DIR_CATALOG', 'D:\zoo/catalog/');
+define('DIR_APPLICATION', 'D:\zoo\zoo\Zoo/admin/');
+define('DIR_SYSTEM', 'D:\zoo\zoo\Zoo/system/');
+define('DIR_DATABASE', 'D:\zoo\zoo\Zoo/system/database/');
+define('DIR_LANGUAGE', 'D:\zoo\zoo\Zoo/admin/language/');
+define('DIR_TEMPLATE', 'D:\zoo\zoo\Zoo/admin/view/template/');
+define('DIR_CONFIG', 'D:\zoo\zoo\Zoo/system/config/');
+define('DIR_IMAGE', 'D:\zoo\zoo\Zoo/image/');
+define('DIR_CACHE', 'D:\zoo\zoo\Zoo/system/cache/');
+define('DIR_DOWNLOAD', 'D:\zoo\zoo\Zoo/download/');
+define('DIR_LOGS', 'D:\zoo\zoo\Zoo/system/logs/');
+define('DIR_CATALOG', 'D:\zoo\zoo\Zoo/catalog/');
 
 // DB
 define('DB_DRIVER', 'mysqli');
--- Zoo/config.php
+++ Zoo/config.php
@@ -6,16 +6,16 @@ define('HTTP_SERVER', 'http://109.87.116.253/');
 define('HTTPS_SERVER', 'http://109.87.116.253/');
 
 // DIR
-define('DIR_APPLICATION', 'D:\zoo/catalog/');
-define('DIR_SYSTEM', 'D:\zoo/system/');
-define('DIR_DATABASE', 'D:\zoo/system/database/');
-define('DIR_LANGUAGE', 'D:\zoo/catalog/language/');
-define('DIR_TEMPLATE', 'D:\zoo/catalog/view/theme/');
-define('DIR_CONFIG', 'D:\zoo/system/config/');
-define('DIR_IMAGE', 'D:\zoo/image/');
-define('DIR_CACHE', 'D:\zoo/system/cache/');
-define('DIR_DOWNLOAD', 'D:\zoo/download/');
-define('DIR_LOGS', 'D:\zoo/system/logs/');
+define('DIR_APPLICATION', 'D:\zoo\zoo\Zoo/catalog/');
+define('DIR_SYSTEM', 'D:\zoo\zoo\Zoo/system/');
+define('DIR_DATABASE', 'D:\zoo\zoo\Zoo/system/database/');
+define('DIR_LANGUAGE', 'D:\zoo\zoo\Zoo/catalog/language/');
+define('DIR_TEMPLATE', 'D:\zoo\zoo\Zoo/catalog/view/theme/');
+define('DIR_CONFIG', 'D:\zoo\zoo\Zoo/system/config/');
+define('DIR_IMAGE', 'D:\zoo\zoo\Zoo/image/');
+define('DIR_CACHE', 'D:\zoo\zoo\Zoo/system/cache/');
+define('DIR_DOWNLOAD', 'D:\zoo\zoo\Zoo/download/');
+define('DIR_LOGS', 'D:\zoo\zoo\Zoo/system/logs/');
 
 // DB
 define('DB_DRIVER', 'mysqli');
